271fae815c0a932107c74bea1a566526298f0002
san0kq <sanrom94@mail.ru>
2023-01-09 19:00:43 +0300

    delete extra line

--- src/views.py
+++ src/views.py
@@ -11,4 +11,3 @@ def my_custom_page_not_found_view(request, exception, template_name='404.html'):
 
 def my_custom_error_view(request, *args, **argv):
     return render(request, '500.html', status=500)
-
